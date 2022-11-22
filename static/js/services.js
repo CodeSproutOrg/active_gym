f5ddeeabad8df9464be854e6884fe590e599af00
vladibuyanov <vladibuyanov@gmail.com>
2022-11-22 17:48:41 +0100

    Version 22.11.2022

---
+++ static/js/services.js
@@ -0,0 +1,7 @@
+let gym = document.getElementById('gym').addEventListener('click', click)
+let programs = document.getElementById('programs').addEventListener('click', click)
+let trainings = document.getElementById('trainings').addEventListener('click', click)
+
+function click() {
+    console.log("Hello world")
+}
