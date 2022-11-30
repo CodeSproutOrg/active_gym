b763fb225874a9ba84dbdd234c710ad74066b83c
vladibuyanov <vladibuyanov@gmail.com>
2022-11-30 18:31:21 +0100

    Version 30.11.2022

--- static/js/services.js
+++ static/js/services.js
@@ -1,7 +1,34 @@
-let gym = document.getElementById('gym').addEventListener('click', click)
-let programs = document.getElementById('programs').addEventListener('click', click)
-let trainings = document.getElementById('trainings').addEventListener('click', click)
+let services = document.querySelectorAll('[class="services-options"]');
 
-function click() {
-    console.log("Hello world")
+for (let i = 0; i < services.length; i++) {
+    let service = services[i].addEventListener(
+        'click',
+        function click(service) {
+            inactive_services(services, i);
+        });
 }
+
+function inactive_services(services, active) {
+    for (let i = 0; i < services.length; i++) {
+        if (i === active) {
+            if (services[active].style.width === "100%") {
+                services[active].style.width = "30%";
+                services[active].style.display = 'flex';
+            }
+            else {
+                services[active].style.width = "100%";
+                services[active].style.display = 'block';
+            }
+        }
+        if (i !== active) {
+            if (services[i].style.display === "none") {
+                services[i].style.display = "flex";
+            }
+            else {
+                services[i].style.display = "none";
+            }
+        }
+    }
+    return inactive
+}
+
